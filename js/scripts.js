75453e6088de3d75ece07444bce056559fc6ddbd
IvanovaNataly <letter4natalia@gmail.com>
2017-06-19 16:00:08 +0300

    init

---
+++ js/scripts.js
@@ -0,0 +1,342 @@
+
+$(document).ready(function(){
+
+/*Horizontal menu turns to vertical*/
+/*On click nav recieves .nav-visible*/
+  var hamburgerButton = document.querySelector("#hamburgerButton");
+  var ddMenu = document.querySelector("#mainMenu");
+
+function menuToggle() {
+    hamburgerButton.classList.toggle('toggle-in');
+    hamburgerButton.classList.toggle('toggle-out');
+
+    if (hamburgerButton.classList.contains("toggle-in")) {
+      ddMenu.classList += " nav-visible"
+    }
+    else {
+      ddMenu.classList = "nav";
+    }
+  }
+
+  hamburgerButton.addEventListener("click", menuToggle);
+  
+//Close menu on click
+  $('.nav-link').on('click', menuToggle);
+
+//menu fixed on scrolling
+	var nav = $('.nav-container');
+    
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 56) {
+            nav.addClass("f-nav");
+        } else {
+            nav.removeClass("f-nav");
+        }
+    });
+
+
+
+
+//carousel
+  $('.carousel').slick(
+  	{
+  		arrows: true
+  	});
+
+// collagePlus-ify it!
+  $('.collage').collagePlus(
+  {
+    /*
+             * The ideal height you want your row to be. It won't set it exactly to this as
+             * plugin adjusts the row height to get the correct width
+             */
+            'targetHeight'    : 250,
+
+            /*
+             * how quickly you want images to fade in once ready can be in ms, "slow" or "fast"
+             * This is only used in the default fade in effect. Timing of the other effects is
+             * controlled in CSS
+             */
+            'fadeSpeed'       : "fast",
+
+            /*
+             * which effect you want to use for revealing the images (note CSS3 browsers only),
+             * Options are effect-1 to effect-6 but you can also code your own
+             * Default is the safest option for supporting older browsers
+             */
+            'effect'          : 'default',
+
+            /*
+             * vertical: effects applied per row to give the impression of descending appearance
+             * horizontal: effects applied in order of appearance in the row to give a horizontal appearance
+             */
+            'direction'       : 'vertical',
+
+            /*
+            * Sometimes there is just one image on the last row and it gets blown up to a huge size to fit the
+            * parent div width. To stop this behaviour, set this to true
+            */
+            'allowPartialLastRow'       : false
+      
+  });
+
+  //Contacts, map buttons
+  $('#placePT').on('click', function(e) {
+      e.preventDefault();
+      $('.places-item').removeClass('place-active');
+      $('#placePT').addClass('place-active');
+      $('.map').hide();
+      $('#mapPT').show();
+  });
+
+  $('#placeRG').on('click', function(e) {
+      e.preventDefault();
+      $('.places-item').removeClass('place-active');
+      $('#placeRG').addClass('place-active');
+      $('.map').hide();
+      $('#mapRG').show();
+  });
+
+  $('#placeBY').on('click', function(e) {
+      e.preventDefault();
+      $('.places-item').removeClass('place-active');
+      $('#placeBY').addClass('place-active');
+      $('.map').hide();
+      $('#mapBY').show();
+  });
+
+
+  // Scroll Animation
+  $("#linkIntro").click(function() {
+  $('html, body').animate({
+      scrollTop: $("#sec-intro").offset().top
+  }, 1500);
+  });
+
+  $("#linkSchedule").click(function() {
+  $('html, body').animate({
+      scrollTop: $("#sec-schedule").offset().top
+  }, 1500);
+  });
+
+  $("#linkPrices").click(function() {
+  $('html, body').animate({
+      scrollTop: $("#sec-subscript").offset().top
+  }, 1500);
+  });
+
+  $("#linkContact").click(function() {
+  $('html, body').animate({
+      scrollTop: $("#sec-contact").offset().top
+  }, 1500);
+  });
+
+});
+
+
+var initPhotoSwipeFromDOM = function(gallerySelector) {
+
+    // parse slide data (url, title, size ...) from DOM elements 
+    // (children of gallerySelector)
+    var parseThumbnailElements = function(el) {
+        var thumbElements = el.childNodes,
+            numNodes = thumbElements.length,
+            items = [],
+            figureEl,
+            linkEl,
+            size,
+            item;
+
+        for(var i = 0; i < numNodes; i++) {
+
+            figureEl = thumbElements[i]; // <figure> element
+
+            // include only element nodes 
+            if(figureEl.nodeType !== 1) {
+                continue;
+            }
+
+            linkEl = figureEl.children[0]; // <a> element
+
+            size = linkEl.getAttribute('data-size').split('x');
+
+            // create slide object
+            item = {
+                src: linkEl.getAttribute('href'),
+                w: parseInt(size[0], 10),
+                h: parseInt(size[1], 10)
+            };
+
+
+
+            if(figureEl.children.length > 1) {
+                // <figcaption> content
+                item.title = figureEl.children[1].innerHTML; 
+            }
+
+            if(linkEl.children.length > 0) {
+                // <img> thumbnail element, retrieving thumbnail url
+                item.msrc = linkEl.children[0].getAttribute('src');
+            } 
+
+            item.el = figureEl; // save link to element for getThumbBoundsFn
+            items.push(item);
+        }
+
+        return items;
+    };
+
+    // find nearest parent element
+    var closest = function closest(el, fn) {
+        return el && ( fn(el) ? el : closest(el.parentNode, fn) );
+    };
+
+    // triggers when user clicks on thumbnail
+    var onThumbnailsClick = function(e) {
+        e = e || window.event;
+        e.preventDefault ? e.preventDefault() : e.returnValue = false;
+
+        var eTarget = e.target || e.srcElement;
+
+        // find root element of slide
+        var clickedListItem = closest(eTarget, function(el) {
+            return (el.tagName && el.tagName.toUpperCase() === 'FIGURE');
+        });
+
+        if(!clickedListItem) {
+            return;
+        }
+
+        // find index of clicked item by looping through all child nodes
+        // alternatively, you may define index via data- attribute
+        var clickedGallery = clickedListItem.parentNode,
+            childNodes = clickedListItem.parentNode.childNodes,
+            numChildNodes = childNodes.length,
+            nodeIndex = 0,
+            index;
+
+        for (var i = 0; i < numChildNodes; i++) {
+            if(childNodes[i].nodeType !== 1) { 
+                continue; 
+            }
+
+            if(childNodes[i] === clickedListItem) {
+                index = nodeIndex;
+                break;
+            }
+            nodeIndex++;
+        }
+
+
+
+        if(index >= 0) {
+            // open PhotoSwipe if valid index found
+            openPhotoSwipe( index, clickedGallery );
+        }
+        return false;
+    };
+
+    // parse picture index and gallery index from URL (#&pid=1&gid=2)
+    var photoswipeParseHash = function() {
+        var hash = window.location.hash.substring(1),
+        params = {};
+
+        if(hash.length < 5) {
+            return params;
+        }
+
+        var vars = hash.split('&');
+        for (var i = 0; i < vars.length; i++) {
+            if(!vars[i]) {
+                continue;
+            }
+            var pair = vars[i].split('=');  
+            if(pair.length < 2) {
+                continue;
+            }           
+            params[pair[0]] = pair[1];
+        }
+
+        if(params.gid) {
+            params.gid = parseInt(params.gid, 10);
+        }
+
+        return params;
+    };
+
+    var openPhotoSwipe = function(index, galleryElement, disableAnimation, fromURL) {
+        var pswpElement = document.querySelectorAll('.pswp')[0],
+            gallery,
+            options,
+            items;
+
+        items = parseThumbnailElements(galleryElement);
+
+        // define options (if needed)
+        options = {
+
+            // define gallery index (for URL)
+            galleryUID: galleryElement.getAttribute('data-pswp-uid'),
+
+            getThumbBoundsFn: function(index) {
+                // See Options -> getThumbBoundsFn section of documentation for more info
+                var thumbnail = items[index].el.getElementsByTagName('img')[0], // find thumbnail
+                    pageYScroll = window.pageYOffset || document.documentElement.scrollTop,
+                    rect = thumbnail.getBoundingClientRect(); 
+
+                return {x:rect.left, y:rect.top + pageYScroll, w:rect.width};
+            }
+
+        };
+
+        // PhotoSwipe opened from URL
+        if(fromURL) {
+            if(options.galleryPIDs) {
+                // parse real index when custom PIDs are used 
+                // http://photoswipe.com/documentation/faq.html#custom-pid-in-url
+                for(var j = 0; j < items.length; j++) {
+                    if(items[j].pid == index) {
+                        options.index = j;
+                        break;
+                    }
+                }
+            } else {
+                // in URL indexes start from 1
+                options.index = parseInt(index, 10) - 1;
+            }
+        } else {
+            options.index = parseInt(index, 10);
+        }
+
+        // exit if index not found
+        if( isNaN(options.index) ) {
+            return;
+        }
+
+        if(disableAnimation) {
+            options.showAnimationDuration = 0;
+        }
+
+        // Pass data to PhotoSwipe and initialize it
+        gallery = new PhotoSwipe( pswpElement, PhotoSwipeUI_Default, items, options);
+        gallery.init();
+    };
+
+    // loop through all gallery elements and bind events
+    var galleryElements = document.querySelectorAll( gallerySelector );
+
+    for(var i = 0, l = galleryElements.length; i < l; i++) {
+        galleryElements[i].setAttribute('data-pswp-uid', i+1);
+        galleryElements[i].onclick = onThumbnailsClick;
+    }
+
+    // Parse URL and open gallery if it contains #&pid=3&gid=1
+    var hashData = photoswipeParseHash();
+    if(hashData.pid && hashData.gid) {
+        openPhotoSwipe( hashData.pid ,  galleryElements[ hashData.gid - 1 ], true, true );
+    }
+};
+
+// execute above function
+initPhotoSwipeFromDOM('.my-gallery');
+
